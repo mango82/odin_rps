e5b8ea0185e5dc48409bd8510ac27b286bfc5387
Richard <mango__82@hotmail.com>
2022-08-31 14:44:10 +1000

    add js plan

---
+++ script.js
@@ -0,0 +1,17 @@
+// Make function getComputerChoice()
+// Function RNG 0-2 convert to strings rock paper scissors
+// return value string
+// Variable computerSelection =  getComputerChoice
+
+// Make function playerSelection()
+// ask for user to type rock paper or scissors
+// convert string to all lowercase letters
+// return converted string
+
+// Make function playRound()
+// Use parameters playerSelection and computer Selection
+// Define game rules R<P<S<R
+
+// Make function game()
+// Call playRound() inside funcion
+// Use loops to keep score until 5 and declares winner
