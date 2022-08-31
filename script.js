2c817df01dff7ea12767f235d1ec8fd95534c25a
Richard <mango__82@hotmail.com>
2022-08-31 15:30:37 +1000

    create getComputerChoice() and playerSelection()

--- script.js
+++ script.js
@@ -1,12 +1,39 @@
 // Make function getComputerChoice()
-// Function RNG 0-2 convert to strings rock paper scissors
-// return value string
+function getComputerChoice(num) {
+// Function RNG 0-2 
+    num = Math.floor(Math.random()*3);
+// convert to strings rock paper scissors return value
+        if (num === 0) {
+            return num = "rock"
+        }
+        else if (num === 1) {
+            return num = "paper"
+        }
+        else if (num === 2) {
+            return num = "scissors"
+        }
+}
 // Variable computerSelection =  getComputerChoice
+let computerSelection = getComputerChoice()
 
 // Make function playerSelection()
+function playerSelection(str) {
 // ask for user to type rock paper or scissors
-// convert string to all lowercase letters
-// return converted string
+    var str = prompt("Type rock paper or scissors.")
+// convert string to all lowercase letters   
+    var str = str.toLowerCase()   
+    
+        if (str === "rock" || str === "paper" || str === "scissors"){
+            return str;
+
+        } else{
+            alert("You have not chosen rock paper or scissors")
+            playerSelection()
+        }
+}
+console.log(playerSelection())
+
+// return converted string if user types rock paper or scissors
 
 // Make function playRound()
 // Use parameters playerSelection and computer Selection
