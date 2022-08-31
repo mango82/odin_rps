e6275bdc8d88ab5c905389c182a70f27fdead79b
Richard <mango__82@hotmail.com>
2022-08-31 16:06:38 +1000

    create playRound()

--- script.js
+++ script.js
@@ -31,12 +31,33 @@ function getPlayerChoice(str) {
             return getPlayerChoice();
         }
 }
-console.log(getPlayerChoice())
-
 
 // Make function playRound()
+function playRound(playerSelection, computerSelection){
 // Use parameters playerSelection and computer Selection
+    var playerSelection = getPlayerChoice()
+    var computerSelection = getComputerChoice()
 // Define game rules R<P<S<R
+        if (playerSelection === computerSelection) {
+            alert("It was a draw! Your opponent chose " + computerSelection)
+        } else if (playerSelection === "rock" && computerSelection === "paper"){
+            alert("You Lose! Paper beats Rock")
+        } else if (playerSelection === "rock" && computerSelection === "scissors"){
+            alert("You Win! Rock beats Scissors")
+
+        } else if (playerSelection === "scissors" && computerSelection === "paper"){
+            alert("You Win! Scissors beat Paper")
+        } else if (playerSelection === "scissors" && computerSelection === "rock"){
+            alert("You Lose! Rock beats Scissors")
+
+        } else if (playerSelection === "paper" && computerSelection === "rock"){
+            alert("You Win! Paper beats Rock")
+        } else if (playerSelection === "paper" && computerSelection === "scissors"){
+            alert("You Lose! Scissors beats Paper")
+        }
+}
+
+playRound()
 
 // Make function game()
 // Call playRound() inside funcion
